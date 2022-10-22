fc5db32497c16ea856e75c230ae4199f03297de3
liubingfang <2906617851@qq.com>
2022-10-22 11:32:26 +0800

    fix:增加假设条件

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ console.log("初始化git");
 console.log("初始dev内容先推一次到main分支")
 console.log("本次需要合并 所有需求分支 到dev 环境");
 console.log("创建远端单独推main分支的需求分支 10012、10013、10014");
-
+console.log('假设本地需求分支已删除且全部提交至git，需要某次commit需求提交至main分支');
 
 
 
